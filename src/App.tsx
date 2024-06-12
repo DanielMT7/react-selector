6f44227bd11e883c193d59e083b636f28d212eec
Daniel Motta <danielmotta04@hotmail.com>
2024-06-12 18:11:57 -0300

    refactor: import Select and create options list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import Select from './components/Select'
+
+import SelectOption from './types/SelectOption'
+
+const options = [
+  { label: 'First', value: 1 },
+  { label: 'Second', value: 2 },
+  { label: 'Third', value: 3 },
+  { label: 'Fourth', value: 4 },
+  { label: 'Fifth', value: 5 }
+]
+
 function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]])
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0])
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={o => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={o => setValue2(o)} />
     </>
   )
 }
